0538468d5baa44eeaf416f43759dc2f6b379a2a6
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-10-16 22:54:40 +0200

    added product card test

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { describe, expect, it } from "vitest";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+
+describe("H1 is in the document", () => {
+  it("should Render h1", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    screen.debug();
+
+    expect(screen.getByText(/abozzi market spesa online/i)).toBeInTheDocument();
+  });
+});
---
+++ src/tests/component/ProductCard.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { describe, expect, it } from "vitest";
+import store from "../../redux/store";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import { MemoryRouter } from "react-router-dom";
+
+describe("ProductCard Component", () => {
+  const product: IProduct = {
+    id: "1234",
+    name: "Nutella",
+    description: "Cioccolato",
+    price: 1.99,
+    quantityAvailable: 7,
+    createdAt: "2024-10-02T00:00:00",
+    lastUpdate: "2024-10-12T16:37:30.864451",
+    imgUrl: "https://example.com/mirko",
+    discountStatus: false,
+    category: {
+      id: "ef0255a1-c7a6-4369-bac0-430979f2d0cb",
+      name: "Dolci",
+      image: "https://example.com/mirkoabozzi",
+    },
+    discountList: [],
+  };
+
+  it("should render product name, description, price, and image", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ProductCard product={product} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Nutella/i)).toBeInTheDocument();
+    expect(screen.getByText(/Cioccolato/i)).toBeInTheDocument();
+    expect(screen.getByText(/1.99/i)).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveAttribute("src", "https://example.com/mirko");
+  });
+});
