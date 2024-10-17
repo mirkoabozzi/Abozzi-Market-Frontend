4a75e180b2ac66c8bd6306bf796c7c60146aad6c
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-10-17 08:32:35 +0200

    added my nav test

---
+++ src/tests/component/MyNav.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { it, expect, describe } from "vitest";
+import MyNav from "../../components/Navbar/MyNav";
+import store from "../../redux/store";
+import { MemoryRouter } from "react-router-dom";
+import { ActionType } from "../../redux/enums/ActionType";
+
+describe("MyNav", () => {
+  it("should show login and registration button if not logged", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <MyNav />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText("Accedi")).toBeInTheDocument();
+    expect(screen.getByText("Registrati")).toBeInTheDocument();
+  });
+
+  it("should show logout button", () => {
+    store.dispatch({ type: ActionType.SET_IS_LOGGED_TRUE });
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <MyNav />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByText(/logout/i)).toBeInTheDocument();
+  });
+});
